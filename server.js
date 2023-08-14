9422d4da800515385c8cb28d6869bdb8a9a6593a
Gabriel Valente <98720580+gabvalente@users.noreply.github.com>
2023-08-14 14:45:49 -0400

    Adapting to vercel

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('body-parser');
 const axios = require('axios');
 const app = express();
 app.use(cors());
