40850582b7407d74f4897597218f80b904562f87
Gabriel Valente <98720580+gabvalente@users.noreply.github.com>
2023-08-14 14:16:56 -0400

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,46 @@
+const poemButton = document.getElementById('generate-poem-button');
+const poemOutput = document.getElementById('poem-output');
+
+async function generatePoemForManu() {
+    const promptMessage = `Escreva um poema curto com rimas em português do Brasil para Manuela, uma criança que completou 7 anos em 8 de Agosto de 2023. 
+    Ela é filha de Rodrigo e Amanda, tem um irmão chamado Rafael e nasceu em 08 de Agosto de 2016 às 13h08min. 
+    Manuela é inteligente, adora os filmes Frozen e Encanto da Disney, ama a natureza, os animais e brincar com seus amigos. 
+    O poema deve ser amigável, ter no máximo 300 caracteres e destacar suas qualidades de criança.
+    Evite palavras difíceis e mantenha o tom do poema alegre e divertido.`;
+
+    try {
+        const poem = await fetchOpenAI(promptMessage);
+        poemOutput.textContent = poem;
+    } catch (error) {
+        console.error("Error generating poem:", error);
+        poemOutput.innerHTML = `<p>Error: ${error.message}</p>`;
+    }
+}
+
+
+async function fetchOpenAI(message) {
+    const response = await fetch('/ask-openai', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message })
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data.message;
+}
+
+poemButton.addEventListener('click', function() {
+    generatePoemForManu()
+        .then(() => {
+            console.log("Poem generated successfully");
+        })
+        .catch(error => {
+            console.error("Error generating poem:", error);
+        });
+});
---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+require('body-parser');
+const axios = require('axios');
+const app = express();
+app.use(cors());
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/ask-openai', async (req, res) => {
+    const userMessage = req.body.message;
+    console.log("Sending prompt to OpenAI:", userMessage);
+
+    try {
+        const openaiResponse = await axios.post('https://api.openai.com/v1/chat/completions', {
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {role: "system", content: "You are a helpful Brazilian poet assistant."},
+                {role: "user", content: userMessage}
+            ]
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const chatGPTResponse = openaiResponse.data.choices[0].message.content.trim();
+        console.log("Received response from OpenAI:", chatGPTResponse);
+        res.json({ message: chatGPTResponse });
+
+    } catch (error) {
+        console.error("Error fetching response from OpenAI:", error.response ? error.response.data : error.message);
+        res.status(500).json({ error: 'Failed to fetch response from OpenAI' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
